1ea6f7cece8e998738d3811626703260933a4796
MakArtKar <makoyan2001@gmail.com>
2022-10-17 18:13:30 +0300

    Fixed bugs in beam search

--- hw_asr/tests/test_text_encoder.py
+++ hw_asr/tests/test_text_encoder.py
@@ -1,6 +1,9 @@
 import unittest
+from collections import defaultdict
 
-from hw_asr.text_encoder.ctc_char_text_encoder import CTCCharTextEncoder
+import torch
+
+from hw_asr.text_encoder.ctc_char_text_encoder import CTCCharTextEncoder, Hypothesis
 
 
 class TestTextEncoder(unittest.TestCase):
@@ -14,5 +17,32 @@ class TestTextEncoder(unittest.TestCase):
         self.assertIn(decoded_text, true_text)
 
     def test_beam_search(self):
-        # TODO: (optional) write tests for beam search
-        pass
+        text_encoder = CTCCharTextEncoder()
+        chars = [
+            ['i', '^'],
+            ['^', 'i'],
+            ['^', ' '],
+            [' ', '^'],
+        ]
+        probs = []
+        for chars_step in chars:
+            probs_step = [0.] * len(text_encoder.char2ind)
+            for c in chars_step:
+                ind = text_encoder.char2ind[c]
+                probs_step[ind] = 1. / len(chars_step)
+            probs.append(probs_step)
+        probs = torch.tensor(probs)
+        beam_search_output = text_encoder.ctc_beam_search(probs, len(probs), beam_size=100)
+
+        result = defaultdict(float)
+        for mask in range(2 ** len(chars)):
+            text = []
+            for i in range(len(chars)):
+                text.append(chars[i][int(mask & (2 ** i) > 0)])
+            text = text_encoder.ctc_decode([text_encoder.char2ind[c] for c in text])
+            result[text] += 1 / 2 ** len(chars)
+        result = [Hypothesis(text=text, prob=prob) for text, prob in result.items()]
+        result = list(sorted(result, key=lambda x: (x.prob, x.text), reverse=True))
+        beam_search_output = list(sorted(beam_search_output, key=lambda x: (x.prob, x.text), reverse=True))
+
+        self.assertEqual(result, beam_search_output[:len(result)])
--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import List, NamedTuple, Iterator
+from typing import List, NamedTuple, Iterator, Tuple
 
 import torch
 
@@ -17,10 +17,10 @@ class CTCCharTextEncoder(CharTextEncoder):
     def __init__(self, alphabet: List[str] = None):
         super().__init__(alphabet)
         vocab = [self.EMPTY_TOK] + list(self.alphabet)
-        self.ind2char = dict(enumerate(vocab))
-        self.char2ind = {v: k for k, v in self.ind2char.items()}
+        self.ind2char: dict[int, str] = dict(enumerate(vocab))
+        self.char2ind: dict[str, int] = {v: k for k, v in self.ind2char.items()}
 
-    def ctc_append(self, text: list[str], last_char: str, new_char: str):
+    def ctc_append(self, text: List[str], last_char: str, new_char: str):
         if new_char != last_char and new_char != self.EMPTY_TOK:
             text.append(new_char)
         return text
@@ -35,11 +35,11 @@ class CTCCharTextEncoder(CharTextEncoder):
         return ''.join(output)
 
     @staticmethod
-    def conditional_prob(prefix_prob, new_char_prob) -> float:
+    def get_prob(prefix_prob, new_char_prob) -> float:
         return prefix_prob * new_char_prob
 
     @staticmethod
-    def merge_hypos(hypos: list[tuple[list[str], str, float]]) -> list[tuple[list[str], str, float]]:
+    def merge_hypos(hypos: List[Tuple[List[str], str, float]]) -> List[Tuple[List[str], str, float]]:
         dict_hypos = defaultdict(float)
         for text, last_char, prob in hypos:
             dict_hypos[(''.join(text), last_char)] += prob
@@ -48,29 +48,31 @@ class CTCCharTextEncoder(CharTextEncoder):
             hypos.append((list(text), last_char, prob))
         return hypos
 
-    def extend_text(self, text: list[str], last_char: str, prob: float, char_probs: torch.Tensor) -> \
-            Iterator[tuple[list[str], str, float]]:
+    def extend_text_by_char(self, text: List[str], last_char: str, prob: float, char_probs: torch.Tensor) -> \
+            Iterator[Tuple[List[str], str, float]]:
         orig_text_length = len(text)
-        for new_char, new_char_prob in zip(self.alphabet, char_probs):
+        for new_char, new_char_prob in zip(self.char2ind, char_probs):
             new_text = self.ctc_append(text, last_char, new_char)
             text = text[:orig_text_length]
             yield (
                 new_text,
                 new_char,
-                self.conditional_prob(prob, new_char_prob)
+                self.get_prob(prob, new_char_prob)
             )
 
     @staticmethod
-    def cut_hypos_number(hypos: list[tuple[list[str], str, float]], beam_size: int) -> list[tuple[list[str], str, float]]:
+    def cut_hypos_number(hypos: List[Tuple[List[str], str, float]], beam_size: int) -> List[Tuple[List[str], str, float]]:
         hypos = list(sorted(hypos, key=lambda x: x[-1], reverse=True))[:beam_size]
         return hypos
 
-    def beam_step(self, hypos: list[tuple[list[str], str, float]], char_probs: torch.Tensor, beam_size: int):
+    def beam_step(self, hypos: List[Tuple[List[str], str, float]], char_probs: torch.Tensor, beam_size: int):
         new_hypos = []
         for text, last_char, prob in hypos:
-            new_hypos.extend(self.extend_text(text, last_char, prob, char_probs))
+            new_hypos.extend(self.extend_text_by_char(text, last_char, prob, char_probs))
+
         new_hypos = self.merge_hypos(new_hypos)
         new_hypos = self.cut_hypos_number(new_hypos, beam_size)
+
         return new_hypos
 
     def ctc_beam_search(self, probs: torch.Tensor, probs_length,
@@ -85,6 +87,9 @@ class CTCCharTextEncoder(CharTextEncoder):
         hypos = [(list(""), self.EMPTY_TOK, 1.)]
         for char_probs in probs:
             hypos = self.beam_step(hypos, char_probs, beam_size)
+        hypos = [(text, '', prob) for text, _, prob in hypos]
+        hypos = self.merge_hypos(hypos)
+        hypos = self.cut_hypos_number(hypos, beam_size)
         hypos = [Hypothesis(text=''.join(text), prob=prob) for text, _, prob in hypos]
         return hypos
 
